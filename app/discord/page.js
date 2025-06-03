3356c7afca6326cb7a9ee663e90e71440e840c40
romeopnz <romeo.pongratz@icloud.com>
2025-06-03 19:35:08 +0200

    Create page.js

---
+++ app/discord/page.js
@@ -0,0 +1,11 @@
+export default function DiscordPage() {
+  return (
+    <div>
+      <h1>Unser Discord</h1>
+      <p>Trete unserer Community bei:</p>
+      <a href="https://discord.gg/dein-einladungslink" target="_blank" rel="noopener noreferrer">
+        Zum Discord beitreten
+      </a>
+    </div>
+  );
+}
