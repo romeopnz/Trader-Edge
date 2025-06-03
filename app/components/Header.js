349fc563517a7a2e91166f900be353d8210ecf58
romeopnz <romeo.pongratz@icloud.com>
2025-06-03 19:22:34 +0200

    Create Header.js

---
+++ app/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ backgroundColor: '#f5f5f5', padding: '1rem 2rem' }}>
+      <h2 style={{ margin: 0 }}>Traders Edge</h2>
+    </header>
+  );
+}
