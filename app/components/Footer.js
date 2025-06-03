c66ec1e38402200bef2c73d9203b8c2d6a9b162b
romeopnz <romeo.pongratz@icloud.com>
2025-06-03 19:33:29 +0200

    Delete app/components/Footer.js

--- app/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#f5f5f5', padding: '1rem 2rem', marginTop: '4rem', textAlign: 'center' }}>
-      <small>© 2025 Traders Edge. Alle Rechte vorbehalten.</small>
-    </footer>
-  );
-}
